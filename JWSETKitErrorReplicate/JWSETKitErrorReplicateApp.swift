25ea48cb2f37c09a9a63d83264540440e48a16f6
Hemant Shrestha <hemant.shrestha@f1soft.com>
2024-08-22 17:31:18 +0545

    Initial Commit

---
+++ JWSETKitErrorReplicate/JWSETKitErrorReplicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  JWSETKitErrorReplicateApp.swift
+//  JWSETKitErrorReplicate
+//
+//  Created by Hemant Shrestha on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct JWSETKitErrorReplicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
